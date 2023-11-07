555920b756636156a464851c626509296dba77fd
Marcus Gajozo <marcusgajozo@gmail.com>
2023-11-07 15:22:58 -0400

    feat: atualizando arquivo data-source.ts

--- src/database/data-source.ts
+++ src/database/data-source.ts
@@ -3,13 +3,13 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export const myDataSource = new DataSource({
-  type: process.env.TYPEORM_CONNECTION || "mysql",
+  type: "postgres",
   host: process.env.TYPEORM_HOST,
   port: parseInt(`${process.env.TYPEORM_PORT}`),
   username: process.env.TYPEORM_USERNAME,
   password: process.env.TYPEORM_PASSWORD,
   database: process.env.TYPEORM_DATABASE,
-  entities: ["src/entities/*.ts"],
+  entities: ["src/app/entities/*.ts"],
   logging: true,
   synchronize: true,
 });
